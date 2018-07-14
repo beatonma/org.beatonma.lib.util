95168abc2a393415f58a82ac2de3f85ad1f13dec
Michael Beaton <beatonma@gmail.com>
2018-07-14 15:16:13 +0100

    Renamed sdkAtLeast() -> atLeast() and made it public

--- src/main/java/org/beatonma/lib/util/Sdk.kt
+++ src/main/java/org/beatonma/lib/util/Sdk.kt
@@ -5,21 +5,21 @@ import android.os.Build
 object Sdk {
 
     val isOreo: Boolean
-        get() = sdkAtLeast(Build.VERSION_CODES.O)
+        get() = atLeast(Build.VERSION_CODES.O)
 
     val isNougat: Boolean
-        get() = sdkAtLeast(Build.VERSION_CODES.N)
+        get() = atLeast(Build.VERSION_CODES.N)
 
     val isMarshmallow: Boolean
-        get() = sdkAtLeast(Build.VERSION_CODES.M)
+        get() = atLeast(Build.VERSION_CODES.M)
 
     val isLollipop: Boolean
-        get() = sdkAtLeast(Build.VERSION_CODES.LOLLIPOP)
+        get() = atLeast(Build.VERSION_CODES.LOLLIPOP)
 
     val isKitkat: Boolean
-        get() = sdkAtLeast(Build.VERSION_CODES.KITKAT)
+        get() = atLeast(Build.VERSION_CODES.KITKAT)
 
-    private fun sdkAtLeast(sdkLevel: Int): Boolean {
+    fun atLeast(sdkLevel: Int): Boolean {
         return Build.VERSION.SDK_INT >= sdkLevel
     }
 }
