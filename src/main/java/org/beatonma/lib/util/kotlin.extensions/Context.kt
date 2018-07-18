36df6f41ceabfcf7200d55438ca7fdf5a8d4d7a9
Michael Beaton <beatonma@gmail.com>
2018-07-18 19:54:25 +0100

    Added extensions for easier access to display metrics

--- src/main/java/org/beatonma/lib/util/kotlin.extensions/Context.kt
+++ src/main/java/org/beatonma/lib/util/kotlin.extensions/Context.kt
@@ -2,6 +2,7 @@ package org.beatonma.lib.util.kotlin.extensions
 
 import android.annotation.TargetApi
 import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.drawable.Drawable
 import android.os.Build.VERSION_CODES.LOLLIPOP
 import android.util.TypedValue
@@ -11,7 +12,8 @@ import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import org.beatonma.lib.util.Sdk
-
+import kotlin.math.max
+import kotlin.math.min
 
 val Context.dp: Float
     get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1F, resources.displayMetrics)
@@ -22,6 +24,10 @@ val Context.dp: Float
 fun Context?.dp(value: Float = 1F): Float {
     if (this == null) return 0F
     return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, resources.displayMetrics)
+}
+
+fun Context.getPrefs(name: String): SharedPreferences {
+    return getSharedPreferences(name, Context.MODE_PRIVATE)
 }
 
 fun Context.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
@@ -77,3 +83,22 @@ val Context?.accentColor: Int?
 
         return color
     }
+
+
+val Context.deviceWidth: Int
+    get() = resources.displayMetrics.widthPixels
+
+val Context.deviceHeight: Int
+    get() = resources.displayMetrics.heightPixels
+
+val Context.maxDimension: Int
+    get() = with (resources.displayMetrics) { return max(widthPixels, heightPixels) }
+
+val Context.minDimension: Int
+    get() = with(resources.displayMetrics) { return min(widthPixels, heightPixels) }
+
+val Context.deviceWidthDp: Int
+    get() = (deviceWidth / resources.displayMetrics.density).toInt()
+
+val Context.deviceHeightDp: Int
+    get() = (deviceHeight / resources.displayMetrics.density).toInt()
