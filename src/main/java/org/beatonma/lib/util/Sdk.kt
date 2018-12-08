43441bb5d70feb56f1e52bbd666fca2302f71c9d
Michael Beaton <beatonma@gmail.com>
2018-12-08 15:26:25 +0000

    Added isPie Added documentation including the numerical value of each Android version.

--- src/main/java/org/beatonma/lib/util/Sdk.kt
+++ src/main/java/org/beatonma/lib/util/Sdk.kt
@@ -3,23 +3,44 @@ package org.beatonma.lib.util
 import android.os.Build
 
 object Sdk {
+    /**
+     * Return true if device is running Android Pie (28) or later
+     */
+    val isPie: Boolean
+        get() = atLeast(Build.VERSION_CODES.P)
 
+    /**
+     * Return true if device is running Android Oreo (26) or later
+     */
     val isOreo: Boolean
         get() = atLeast(Build.VERSION_CODES.O)
 
+    /**
+     * Return true if device is running Android Nougat (24) or later
+     */
     val isNougat: Boolean
         get() = atLeast(Build.VERSION_CODES.N)
 
+    /**
+     * Return true if device is running Android Marshmallow (23) or later
+     */
     val isMarshmallow: Boolean
         get() = atLeast(Build.VERSION_CODES.M)
 
+    /**
+     * Return true if device is running Android Lollipop (21) or later
+     */
     val isLollipop: Boolean
         get() = atLeast(Build.VERSION_CODES.LOLLIPOP)
 
+    /**
+     * Return true if device is running Android Kitkat (19) or later
+     */
     val isKitkat: Boolean
         get() = atLeast(Build.VERSION_CODES.KITKAT)
 
-    fun atLeast(sdkLevel: Int): Boolean {
-        return Build.VERSION.SDK_INT >= sdkLevel
-    }
+    /**
+     * Return true if device is running a version of Android that is at least as recent as [sdkLevel]
+     */
+    fun atLeast(sdkLevel: Int) = Build.VERSION.SDK_INT >= sdkLevel
 }
